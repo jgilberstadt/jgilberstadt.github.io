ba2bb81193c5973648a1ddc8cf438d1bbf19c59c
jgilberstadt <51213764+jgilberstadt@users.noreply.github.com>
2025-12-17 15:46:49 -0600

    Delete assets/js/header.js

--- assets/js/header.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  fetch("partials/header.html")
-    .then(response => response.text())
-    .then(html => {
-      document.getElementById("site-header").innerHTML = html;
-      setupMenu();
-    })
-    .catch(err => console.error("Failed to load header:", err));
-});
