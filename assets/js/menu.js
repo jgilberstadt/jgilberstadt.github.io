d3cc61095a4176ff83928336abc2b6bfe5a134b0
jgilberstadt <51213764+jgilberstadt@users.noreply.github.com>
2025-12-25 23:18:56 -0600

    Added code to load sw.js file

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -263,3 +263,12 @@ function triggerPageFadeIn() {
 if (document.readyState === "interactive" || document.readyState === "complete") {
   triggerPageFadeIn();
 }
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/sw.js")
+      .then((reg) => console.log("Service Worker registered!"))
+      .catch((err) => console.log("Service Worker failed:", err));
+  });
+}
